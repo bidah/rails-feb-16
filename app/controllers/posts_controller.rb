24b0106fc12862286f76abbd926be0a2a142584b
github username <github email address>
2015-02-16 12:05:30 -0300

    form working with strong params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,18 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    # @post = Post.create(title: params[:post][:title], content: params[:post][:content])
+    @post = Post.create(post_params)
+
+    redirect_to post_path(@post)
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
@@ -18,5 +24,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,9 @@
 Rails.application.routes.draw do
-  get 'posts/index'
 
-  get 'posts/new'
 
-  get 'posts/create'
+  resources :posts
 
-  get 'posts/show'
-
-  get 'posts/edit'
-
-  get 'posts/update'
-
-  get 'posts/destroy'
-
-  resources :post
-
-  root 'post#index'
+  root 'posts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
