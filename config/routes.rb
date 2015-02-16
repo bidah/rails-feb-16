d6b7702b40e0005ae7ceedf81abc359b90beb571
github username <github email address>
2015-02-16 11:22:10 -0300

    added posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'posts/new'
+
+  get 'posts/create'
+
+  get 'posts/show'
+
+  get 'posts/edit'
+
+  get 'posts/update'
+
+  get 'posts/destroy'
+
   resources :post
 
   root 'post#index'
